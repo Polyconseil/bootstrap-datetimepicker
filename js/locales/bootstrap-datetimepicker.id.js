187620640ba9b443a32e9eb4faf6fd49797045e6
Arif Kurniawan <arifk97@gmail.com>
2015-04-20 10:12:11 +0700

    Add 'today' translation for Bahasa

--- js/locales/bootstrap-datetimepicker.id.js
+++ js/locales/bootstrap-datetimepicker.id.js
@@ -10,7 +10,7 @@
 		daysMin: ["Mg", "Sn", "Sl", "Ra", "Ka", "Ju", "Sa", "Mg"],
 		months: ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"],
 		monthsShort: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Ags", "Sep", "Okt", "Nov", "Des"],
-		today: "Hari ini",
+		today: "Hari Ini",
 		suffix: [],
 		meridiem: [],
 		weekStart: 1,
